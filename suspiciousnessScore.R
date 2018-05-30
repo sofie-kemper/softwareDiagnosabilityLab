793c04d4665d9149e6111a0dd671ed36e6fc009c
Sofie Kemper <sofie.kemper@gmail.com>
2018-05-30 11:51:46 +0200

    Adapt to new spectrum format

--- suspiciousnessScore.R
+++ suspiciousnessScore.R
@@ -10,15 +10,17 @@ ARTIFACT.LEVEL <- "method"
 
 project.data <- data.table(project = c("Chart", "Closure", "Lang", "Math", "Mockito", "Time"),
                            nr.bugs = c(26, 133, 65, 106, 38, 27))
-#project.data[3,2] <- 33
+
+real.faults <- fread("coverageData/realFaults/faults.csv", header = T)
 
 for(VERSION in 1:project.data[project == PROJECT, nr.bugs]){
 
   DATA.PATH <- file.path("coverageData", ARTIFACT.LEVEL, PROJECT, VERSION)
 
-  spectra <- fread(file.path(DATA.PATH, "spectra"), header = T, sep = "\t")
+  spectra <- fread(file.path(DATA.PATH, "spectra"), header = F, sep = "\t")
   matrix <- fread(file.path(DATA.PATH, "matrix"), header = F, sep = " ")
-  colnames(matrix) <- unlist(c(spectra$Component, "pass.fail"))
+  colnames(matrix) <- unlist(c(spectra[,1], "pass.fail"))
+  colnames(spectra) <- c("Component")
   ## matrix: rows = tests, cols = code artifacts + 1 row for pass/fail
 
   scores <- compute.suspiciousness.scores(matrix, spectra)
@@ -26,5 +28,8 @@ for(VERSION in 1:project.data[project == PROJECT, nr.bugs]){
   write.csv(scores, file.path(DATA.PATH, "suspiciousness.csv"), row.names = F)
 
   jaccard <- calculate_suspiciousness(scores, type = "Jaccard", threshold = 2)
+
+  score <- score_accuracy(jaccard,
+                          real.fault = real.faults[id == paste(PROJECT, VERSION, sep="_"), faulty.method])
 
 }
