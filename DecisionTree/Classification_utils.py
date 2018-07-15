a3f1a7476c256597c2b6656eca33075b9f4b438b
Thomas Dornberger <ga84bib@mytum.de>
2018-07-15 17:41:40 +0200

    update classification_utils script to last version

--- DecisionTree/Classification_utils.py
+++ DecisionTree/Classification_utils.py
@@ -4,8 +4,11 @@ Created on Fri Jul  6 18:11:06 2018
 
 @author: SeRvU
 """
-def get_class_label_names():
-    return ['perfect', 'good', 'medium', 'bad']
+def get_class_names():
+    return ['perfect', 'good', 'medium', 'useless']
+
+def get_coarse_grained_class_names():
+    return ['useful', 'useless']
 
 def get_class_labels(targets):
     classLabels = []
@@ -16,17 +19,32 @@ def get_class_labels(targets):
         if(entry > 1 and entry < 5):
             classLabels.append(1)
             continue
-        if(entry >= 5 and entry < 9):
+        if(entry >= 5 and entry < 12):
             classLabels.append(2)
             continue
-        if(entry >= 9):
+        if(entry >= 12):
             classLabels.append(3)
             continue
         print('ERROR: no class label could be assigned')
         return(None)
     return(classLabels)
-    
+
+""" prints an iterable, e.g. a list. Prevents that elements are skipped with '...'
+    in large lists.         """
 def print_iterable(targets):
     for entry in targets:
         print(entry, end=' ')
     print('',end='\n')
+    
+def get_class_labels_coarse_grained(targets):
+    classLabels = []
+    for entry in targets:
+        if(entry <= 11):
+            classLabels.append(0)
+            continue
+        if(entry >= 12):
+            classLabels.append(1)
+            continue
+        print('ERROR: no class label could be assigned')
+        return(None)
+    return(classLabels)
